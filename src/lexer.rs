6b0f1555e408d2a5b3c67897d67f2a17ab471209
ikanago <28985004+ikanago@users.noreply.github.com>
2020-05-20 22:44:29 +0900

    ✨ Lex keyword

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,19 @@
+use std::collections::HashMap;
 use std::str::from_utf8;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum Token {
     Number(usize),
     Identifier(String),
     Plus,
     Asterisk,
+    If,
+}
+
+fn reserve_keyword() -> HashMap<String, Token> {
+    let mut keywords = HashMap::new();
+    keywords.insert("if".to_string(), Token::If);
+    keywords
 }
 
 #[derive(Debug)]
@@ -15,6 +23,7 @@ pub enum LexError {}
 pub struct Lexer<'a> {
     input: &'a [u8],
     pos: usize,
+    keywords: HashMap<String, Token>,
     pub tokens: Vec<Token>,
 }
 
@@ -23,6 +32,7 @@ impl<'a> Lexer<'a> {
         Self {
             input: input.as_bytes(),
             pos: 0,
+            keywords: reserve_keyword(),
             tokens: Vec::new(),
         }
     }
@@ -62,9 +72,12 @@ impl<'a> Lexer<'a> {
     fn lex_identifier(&mut self) -> Token {
         let start = self.pos;
         let end = self.read_many(|b| b.is_ascii_alphanumeric() || b == b'_');
-        let identifier = from_utf8(&self.input[start..end]).unwrap().to_string();
         self.pos = end;
-        Token::Identifier(identifier)
+        let identifier = from_utf8(&self.input[start..end]).unwrap().to_string();
+        match self.keywords.get(&identifier) {
+            Some(keyword) => keyword.clone(),
+            None => Token::Identifier(identifier),
+        }
     }
 
     fn read_many(&self, satisfy: impl Fn(u8) -> bool) -> usize {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use mini_ml::lexer;
 
 fn main() {
-    let mut lexer = lexer::Lexer::new("123+456*789+aa3_7f");
+    let mut lexer = lexer::Lexer::new("if+123+456*789+aa3_7f");
     let tokens = lexer.lex();
     println!("{:?}", tokens);
 }
